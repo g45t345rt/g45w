add69169f50462bcb46e97e499616c1d469a4319
secretnamebasis <secretnamebasis@keemail.me>
2023-12-01 04:37:15 -0700

    minor: edit build scripts

--- build_vars.sh
+++ build_vars.sh
@@ -1,11 +1,11 @@
-VERSION="0.8.0"
+VERSION="0.0.1"
 VERSION_INCREMENT=1
 NAME=secret-wallet
 
-FLAGS="-X github.com/g45t345rt/g45w/settings.Version=v$VERSION"
-FLAGS="$FLAGS -X github.com/g45t345rt/g45w/settings.BuildTime=$(date +%s)"
-FLAGS="$FLAGS -X github.com/g45t345rt/g45w/settings.GitVersion=$(git describe --tags --dirty --always)"
+FLAGS="-X github.com/secretnamebasis/secret-wallet/settings.Version=v$VERSION"
+FLAGS="$FLAGS -X github.com/secretnamebasis/secret-wallet/settings.BuildTime=$(date +%s)"
+FLAGS="$FLAGS -X github.com/secretnamebasis/secret-wallet/settings.GitVersion=$(git describe --tags --dirty --always)"
 
-APPID="com.github.g45t345rt.g45w"
+APPID="com.github.secretnamebasis.secret_wallet"
 
 export FLAGS APPID VERSION VERSION_INCREMENT
