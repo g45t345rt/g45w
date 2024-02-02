bdfa25d3d7f69750e76c02eca6636342f4acd311
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-02-02 16:09:11 -0500

    pulse animation if status is yellow

--- containers/node_status_bar/node_status_bar.go
+++ containers/node_status_bar/node_status_bar.go
@@ -7,11 +7,13 @@ import (
 
 	"gioui.org/io/pointer"
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"github.com/g45t345rt/g45w/animation"
 	"github.com/g45t345rt/g45w/app_instance"
 	"github.com/g45t345rt/g45w/integrated_node"
 	"github.com/g45t345rt/g45w/lang"
@@ -20,12 +22,15 @@ import (
 	page_node "github.com/g45t345rt/g45w/pages/node"
 	"github.com/g45t345rt/g45w/theme"
 	"github.com/g45t345rt/g45w/wallet_manager"
+	"github.com/tanema/gween"
+	"github.com/tanema/gween/ease"
 )
 
 type NodeStatusBar struct {
 	clickable            *widget.Clickable
 	IntegratedNodeStatus *integrated_node.NodeStatus
 	RemoteNodeInfo       *page_node.RemoteNodeInfo
+	pulseAnimation       *animation.Animation
 }
 
 var Instance *NodeStatusBar
@@ -35,6 +40,10 @@ func LoadInstance() *NodeStatusBar {
 		clickable:            new(widget.Clickable),
 		IntegratedNodeStatus: integrated_node.NewNodeStatus(1 * time.Second),
 		RemoteNodeInfo:       page_node.NewRemoteNodeInfo(3 * time.Second),
+		pulseAnimation: animation.NewAnimation(false, gween.NewSequence(
+			gween.New(1, .7, .5, ease.OutBounce),
+			gween.New(.7, 1, .5, ease.InBounce),
+		)),
 	}
 	Instance = nodeStatusBar
 	return nodeStatusBar
@@ -63,8 +72,9 @@ func (n *NodeStatusBar) Layout(gtx layout.Context, th *material.Theme) layout.Di
 
 	wallet := wallet_manager.OpenedWallet
 	currentNode := node_manager.CurrentNode
-	status := "unassigned node"
+	status := lang.Translate("Unassigned Node")
 	statusDotColor := theme.Current.NodeStatusDotRedColor // color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	nodeName := ""
 
 	if currentNode != nil {
 		if currentNode.Integrated {
@@ -74,20 +84,22 @@ func (n *NodeStatusBar) Layout(gtx layout.Context, th *material.Theme) layout.Di
 			//bestHeight := n.integratedNodeStatus.BestHeight
 			walletHeight := wallet.Memory.Get_Height()
 			daemonHeight := wallet.Memory.Get_Daemon_Height()
-			out := n.IntegratedNodeStatus.PeerOutCount
+			//out := n.IntegratedNodeStatus.PeerOutCount
 
 			if walletHeight < daemonHeight {
-				statusDotColor = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
+				statusDotColor = theme.Current.NodeStatusDotYellowColor
 			} else {
-				statusDotColor = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+				statusDotColor = theme.Current.NodeStatusDotGreenColor
 			}
 
-			status = fmt.Sprintf("%d / %d - %dP (%s)", walletHeight, daemonHeight, out, lang.Translate("Integrated Node"))
+			nodeName = lang.Translate("Integrated Node")
+			status = fmt.Sprintf("%d / %d", walletHeight, daemonHeight)
 		} else {
 			n.RemoteNodeInfo.Active()
 			walletHeight := wallet.Memory.Get_Height()
 			daemonHeight := wallet.Memory.Get_Daemon_Height()
-			out := n.RemoteNodeInfo.Result.Outgoing_connections_count
+			//out := n.RemoteNodeInfo.Result.Outgoing_connections_count
+			nodeName = currentNode.Name
 
 			if n.RemoteNodeInfo.Err == nil {
 				if walletHeight < daemonHeight {
@@ -96,16 +108,16 @@ func (n *NodeStatusBar) Layout(gtx layout.Context, th *material.Theme) layout.Di
 					statusDotColor = theme.Current.NodeStatusDotGreenColor // color.NRGBA{R: 0, G: 255, B: 0, A: 255}
 				}
 
-				status = fmt.Sprintf("%d / %d - %dP (%s)", walletHeight, daemonHeight, out, currentNode.Name)
+				status = fmt.Sprintf("%d / %d", walletHeight, daemonHeight)
 			} else {
-				status = fmt.Sprintf("%s (%s)", lang.Translate("Disconnected"), currentNode.Name)
+				status = lang.Translate("Disconnected")
 			}
 		}
 	}
 
 	if n.clickable.Clicked(gtx) {
 		app_instance.Router.SetCurrent(pages.PAGE_NODE)
-		//op.InvalidateOp{}.Add(gtx.Ops)
+		op.InvalidateOp{}.Add(gtx.Ops)
 	}
 
 	return n.clickable.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
@@ -118,16 +130,42 @@ func (n *NodeStatusBar) Layout(gtx layout.Context, th *material.Theme) layout.Di
 			Left: unit.Dp(10), Right: unit.Dp(10),
 		}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 			return layout.Flex{
+				Axis:      layout.Horizontal,
 				Alignment: layout.Middle,
 			}.Layout(gtx,
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					return StatusDot{
+					// pulse if yellow
+					if statusDotColor == theme.Current.NodeStatusDotYellowColor {
+						n.pulseAnimation.Start()
+					} else {
+						n.pulseAnimation.Reset()
+					}
+
+					r := op.Record(gtx.Ops)
+					dims := StatusDot{
 						Color: statusDotColor,
 					}.Layout(gtx)
+					c := r.Stop()
+					gtx.Constraints.Min = dims.Size
+
+					n.pulseAnimation.Start()
+					state := n.pulseAnimation.Update(gtx)
+					if state.Active {
+						defer animation.TransformScaleCenter(gtx, state.Value).Push(gtx.Ops).Pop()
+					}
+
+					c.Add(gtx.Ops)
+					return dims
+				}),
+				layout.Rigid(layout.Spacer{Width: unit.Dp(10)}.Layout),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					lbl := material.Label(th, unit.Sp(16), status)
+					lbl.Color = theme.Current.NodeStatusTextColor
+					return lbl.Layout(gtx)
 				}),
 				layout.Rigid(layout.Spacer{Width: unit.Dp(10)}.Layout),
 				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					lbl := material.Label(th, unit.Sp(16), status)
+					lbl := material.Label(th, unit.Sp(16), nodeName)
 					lbl.Color = theme.Current.NodeStatusTextColor
 					return lbl.Layout(gtx)
 				}),
